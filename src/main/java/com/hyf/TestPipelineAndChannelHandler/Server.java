42a39e8f77fb4aaea5fbc197086a6127aa77dcc8
Howinfun <876237770@qq.com>
2019-07-01 15:28:03 +0800

    注释

--- src/main/java/com/hyf/TestPipelineAndChannelHandler/Server.java
+++ src/main/java/com/hyf/TestPipelineAndChannelHandler/Server.java
@@ -28,9 +28,10 @@ public class Server {
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         /**
                          * 1、需要在不变Handler的添加顺序，打印这样：inboundA -> inboundC -> outboundB -> outboundA
+                         *     解决方案：给Handler增加名字，可根据名字热插拔Handler，ctx.piepeline.remove(String var);
                          * 2、每个Handler输入上一个Handler的结束时间
+                         *     解决方案：使用Channel.attr().set() 和 Channel.attr().get()
                          */
-                        // 给Handler增加名字，可根据名字热插拔Handler
                         ch.pipeline().addLast("in_A",new InBoundHandlerA())
                                 .addLast("in_B",new InBoundHandlerB())
                                 .addLast("in_C",new InBoundHandlerC())
